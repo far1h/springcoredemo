10d0560266b313d0fa4274f441af85663bfff9d1
far1h <farihmuhammad45@gmail.com>
2024-10-07 17:13:00 +0700

    Initial commit

---
+++ src/test/java/com/farihmhmd/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farihmhmd.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
