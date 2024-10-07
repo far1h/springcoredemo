f296272332bc13ba8ba94a47113221a13602fe22
far1h <farihmuhammad45@gmail.com>
2024-10-07 18:49:47 +0700

    Explicitly list base packages to scan

--- src/main/java/com/farihmhmd/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/farihmhmd/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,10 @@ package com.farihmhmd.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.farihmhmd.springcoredemo",
+							"com.farihmhmd.util"}
+)
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
