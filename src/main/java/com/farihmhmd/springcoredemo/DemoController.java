255d81483cf3aaa8882d3fbd7fb3633399616aee
far1h <farihmuhammad45@gmail.com>
2024-10-07 17:23:27 +0700

    Create a constructor in controller for injections

--- src/main/java/com/farihmhmd/springcoredemo/DemoController.java
+++ src/main/java/com/farihmhmd/springcoredemo/DemoController.java
@@ -1,8 +1,16 @@
 package com.farihmhmd.springcoredemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
 
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
 }
