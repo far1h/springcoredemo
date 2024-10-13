69a95d9a43d3f9d58170af9b8376b98233fbfdf8
far1h <farihmuhammad45@gmail.com>
2024-10-13 17:53:53 +0700

    Add printin to constructors

--- src/main/java/com/farihmhmd/springcoredemo/rest/DemoController.java
+++ src/main/java/com/farihmhmd/springcoredemo/rest/DemoController.java
@@ -13,6 +13,7 @@ public class DemoController {
 
     @Autowired
     public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
