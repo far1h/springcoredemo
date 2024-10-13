32e6baa909070996dc3049ba70d4d0b81537a5e8
far1h <farihmuhammad45@gmail.com>
2024-10-13 17:27:57 +0700

    Specify bean using Qualifier

--- src/main/java/com/farihmhmd/springcoredemo/rest/DemoController.java
+++ src/main/java/com/farihmhmd/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.farihmhmd.springcoredemo.rest;
 
 import com.farihmhmd.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController(@Qualifier("trackCoach") Coach theCoach){
         myCoach = theCoach;
     }
 
