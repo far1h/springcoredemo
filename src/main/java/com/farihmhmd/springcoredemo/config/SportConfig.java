274df288996fc48eb3237817ab18a32ddaafa3a3
far1h <farihmuhammad45@gmail.com>
2024-10-14 09:10:14 +0700

    Define @Bean method to configure the bean

--- src/main/java/com/farihmhmd/springcoredemo/config/SportConfig.java
+++ src/main/java/com/farihmhmd/springcoredemo/config/SportConfig.java
@@ -1,7 +1,15 @@
 package com.farihmhmd.springcoredemo.config;
 
+import com.farihmhmd.springcoredemo.common.Coach;
+import com.farihmhmd.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SportConfig {
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach();
+    }
 }
